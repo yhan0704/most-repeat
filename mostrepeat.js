fa23af61697c1f39d575a613a092cbb74aee0c98
github username <github email address>
2020-04-07 14:13:20 -0400

    done

--- mostrepeat.js
+++ mostrepeat.js
@@ -1,16 +1,13 @@
 function repeated(arr){
     repeatedObj = {}
-    emptyArray = []
 	for(let i = 0; i < arr.length; i++){
-       if(!repeatedObj[arr[i]]){
-           repeatedObj[arr[i]] = 1
-       }else{
+       if(repeatedObj[arr[i]]){
            repeatedObj[arr[i]]++
+       }else{
+           repeatedObj[arr[i]] = 1
+           console.log(Object.keys(repeatedObj))
        }
     }
-
-    // let arr = Object.values(obj);
-    // let min = Math.min(...arr);
 
     return Object.keys(repeatedObj).reduce((a, b) => repeatedObj[a] > repeatedObj[b] ? a : b )
 }
